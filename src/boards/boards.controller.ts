27e2cc0eaf7dc57584901aa425fe1005c4f58996
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-08-20 13:32:50 +0900

    feat: 게시물 생성 기능 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get()
-  public getAllBoard() {
+  public getAllBoard(): Board[] {
     return this.boardsService.getAllBoard();
+  }
+
+  @Post()
+  public createBoard(
+    @Body('title') title,
+    @Body('description') description,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-  private boards = []; //TODO: DB로 교체
+  private boards: Board[] = []; //TODO: DB로 교체
 
-  public getAllBoard() {
+  public getAllBoard(): Board[] {
     return this.boards;
+  }
+
+  public createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
