9ba4d3e94fef92a85e542d644a76f761ccaef8be
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-08-20 20:36:33 +0900

    chore: typeOrm 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
-  controllers: [],
-  providers: [],
+  imports: [BoardsModule, TypeOrmModule.forRoot(typeORMConfig)],
 })
 export class AppModule {}
